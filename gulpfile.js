36dfdfedb9a88a23721e7b145ef34d93928430b9
Artur Vasconcelos <artur.vasconcelos8@gmail.com>
2023-09-29 16:43:28 -0400

    executando tarefas em paralelo

--- gulpfile.js
+++ gulpfile.js
@@ -2,19 +2,23 @@ const gulp = require("gulp");
 
 function funcaoPadrao(callback) {
   /* callback -> gulp injetará uma função */
-  console.log("Executando via Gulp");
-  callback();
+  setTimeout(() => {
+    console.log("Executando via Gulp");
+    callback();
+  }, 2500);
 }
 
 function cumprimento(callback) {
-  console.log("Olá, Gulp!");
-  dizTchau();
-  callback();
+  setTimeout(() => {
+    console.log("Olá, Gulp!");
+    dizTchau();
+    callback();
+  }, 3500);
 }
 
 function dizTchau() {
   console.log("Tchau, Gulp!");
 }
 
-exports.default = gulp.series(funcaoPadrao, cumprimento);
+exports.default = gulp.parallel(funcaoPadrao, cumprimento);
 exports.cumprimento = cumprimento;
