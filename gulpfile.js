b924afdb852836380dff5064c61fbeab68fe3558
Artur Vasconcelos <artur.vasconcelos8@gmail.com>
2023-09-29 17:11:13 -0400

    criado tarefa para compilar o `sass`

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,26 @@
 const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+// pacote 'gulp-sass' é responsável por integrar o sass com o gulp
+// o pacote 'sass' que fará toda a compilação
+
+/* 
+#funções
+- 'src' -> recebe como parametro os arquivos e as pastas do codigo sass;
+- 'pipe' -> tem as papel de encadear as funções;
+- 'dest' -> para onde os arquivos seram destinados.
+
+#parâmetros
+- callback -> gulp injetará uma função
+*/
+
+function compliaSass() {
+  return gulp
+    .src("./source/styles/main.scss")
+    .pipe(sass())
+    .pipe(gulp.dest("./build/styles"));
+}
 
 function funcaoPadrao(callback) {
-  /* callback -> gulp injetará uma função */
   setTimeout(() => {
     console.log("Executando via Gulp");
     callback();
@@ -22,3 +41,4 @@ function dizTchau() {
 
 exports.default = gulp.parallel(funcaoPadrao, cumprimento);
 exports.cumprimento = cumprimento;
+exports.sass = compliaSass;
